78a0c05cbf61c400982501d3031b5f3849c4eadc
Tatsinnit <tatsatmishra@gmail.com>
2020-06-11 10:45:45 +1200

    Fix handling of dubuious responses from App Lens API (#26)

--- src/commands/detectorDiagnostics/helpers/networkconnectivityhtmlhelper.ts
+++ src/commands/detectorDiagnostics/helpers/networkconnectivityhtmlhelper.ts
@@ -5,17 +5,20 @@ export function convertHtmlJsonConfiguration(
   webviewClusterData: AppLensARMResponse["properties"],
   datasetIndex: number
 ): any {
-  // API returns thml embeded in json.
-  const htmlRawDataArray = webviewClusterData.dataset[datasetIndex].table.rows;
+  // ARM call returns html embed in json and at times some index values are not guranteed.
+  if (webviewClusterData.dataset.length >= datasetIndex) {
+    const htmlRawDataArray = webviewClusterData.dataset[datasetIndex].table?.rows;
 
-  const htmlJsonDataSet = {
-    subnet: {
-      subnetClass: htmlRawDataArray[0][0].toString().toLowerCase(),
-      subnetDataset: htmlRawDataArray
-    }
-  };
+    const htmlJsonDataSet = {
+      subnet: {
+        subnetClass: htmlRawDataArray[0][0].toString().toLowerCase(),
+        subnetDataset: htmlRawDataArray
+      }
+    };
+    return htmlJsonDataSet;
+  }
 
-  return htmlJsonDataSet;
+  return undefined;
 }
 
 export function htmlHandlerRegisterHelper() {
