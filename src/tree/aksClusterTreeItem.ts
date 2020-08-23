d66b33c622b55fd0a0451aa2f1cdb878af386fc5
Tatsinnit <tatsatmishra@gmail.com>
2020-08-24 11:25:49 +1200

    Enable kubernetes icon appear for Azure Clusters.

--- src/tree/aksClusterTreeItem.ts
+++ src/tree/aksClusterTreeItem.ts
@@ -3,6 +3,9 @@ import { Resource } from "azure-arm-storage/lib/models";
 import { CloudExplorerV1 } from "vscode-kubernetes-tools-api";
 import { SubscriptionClient } from 'azure-arm-resource';
 import { toSubscription } from "../azure-api-utils";
+import { getExtensionPath } from "../commands/utils/host";
+import * as path from 'path';
+import * as vscode from 'vscode';
 
 // The de facto API of tree nodes that represent individual AKS clusters.
 // Tree items should implement this interface to maintain backward compatibility with previous versions of the extension.
@@ -18,12 +21,15 @@ export default class AksClusterTreeItem extends AzureTreeItem implements AksClus
     constructor(
         parent: AzExtParentTreeItem,
         private readonly resource: Resource) {
-            super(parent);
+        super(parent);
 
-            // TODO: Set fancy icon for AKS cluster nodes.
-
-            this.id = this.resource.id;
+        // TODO: Set fancy icon for AKS cluster nodes for Google Cloud and AWS.
+        // For now this carefully enables AKS Icons for Azure Clsuter.
+        if (this.nodeType === "cluster" && this.root.environment.name === "Azure" && this.contextValue.includes("aks.cluster")) {
+            this.iconPath = vscode.Uri.file(path.join(getExtensionPath()!, 'resources', 'k8s-logo.png'));
         }
+        this.id = this.resource.id;
+    }
 
     public readonly contextValue: string = `aks.cluster ${CloudExplorerV1.SHOW_KUBECONFIG_COMMANDS_CONTEXT}`;
 
