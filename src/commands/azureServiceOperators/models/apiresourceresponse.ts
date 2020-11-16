79242bef7d69d9ebb2944e2972e6f85207cbaf3a
Tatsinnit <>
2020-11-17 01:29:14 +1300

    ASO command along with Pin and UnPin + Basic structure.

---
+++ src/commands/azureServiceOperators/models/apiresourceresponse.ts
@@ -0,0 +1,8 @@
+export interface APIResourceResponce {
+    readonly displayName: string;
+    // readonly shortnames: string;
+    // readonly apigroup: string;
+    // readonly namespaced: string;
+    readonly kind: string;
+    readonly resourceType: string;
+   }
