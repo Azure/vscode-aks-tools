4448c6c1e967eb0371f03fa007a5ba00bdaf64a3
Tatsinnit <tamishra@microsoft.com>
2021-02-23 16:10:53 +1300

    Errorable handle from a layer below.

--- src/commands/azureServiceOperators/ui/azureservicebrowser.ts
+++ src/commands/azureServiceOperators/ui/azureservicebrowser.ts
@@ -1,10 +1,15 @@
 import * as k8s from 'vscode-kubernetes-tools-api';
 import * as vscode from 'vscode';
+import { Errorable } from '../../utils/errorable';
 
 interface AzureServiceKind {
     readonly displayName: string;
     readonly manifestKind: string;
     readonly abbreviation: string;
+}
+
+enum ASOInstallation {
+    ASONotInstalled = 'ASONotInstalled'
 }
 
 export async function AzureServiceBrowser(explorer: k8s.ClusterExplorerV1): Promise<k8s.ClusterExplorerV1.NodeContributor> {
@@ -16,25 +21,33 @@ export async function AzureServiceBrowser(explorer: k8s.ClusterExplorerV1): Prom
 }
 
 async function allServiceKinds(): Promise<AzureServiceKind[] | undefined> {
+    const apiResult = await getAPIResourceCommandResult();
+
+    if (apiResult.succeeded) {
+        return apiResult.result;
+    } else if (apiResult.error !== ASOInstallation.ASONotInstalled) {
+        vscode.window.showWarningMessage(apiResult.error);
+    }
+    return undefined;
+}
+
+async function getAPIResourceCommandResult(): Promise<Errorable<AzureServiceKind[]>> {
     const kubectl = await k8s.extension.kubectl.v1;
 
     if (!kubectl.available) {
-        vscode.window.showWarningMessage(`Kubectl is unavailable.`);
-        return undefined;
+        return { succeeded: false, error: `Kubectl is unavailable.` };
     }
     const asoAPIResourceCommandResult = await kubectl.api.invokeCommand("api-resources --api-group azure.microsoft.com --no-headers");
 
-    if (!asoAPIResourceCommandResult) { // Fail to invoke command
-        vscode.window.showWarningMessage(`Azure Service Operator api-resources failed to invoke command.`);
-        return undefined;
+    if (!asoAPIResourceCommandResult) { // Fail to invoke command.
+        return { succeeded: false, error: `Azure Service Operator api-resources failed to invoke command.` };
     } else if (asoAPIResourceCommandResult.code !== 0 && !asoAPIResourceCommandResult.stdout) { // Error result and Faild execution.
-        vscode.window.showWarningMessage(`Azure Service Operator api-resources command failed with following error: ${asoAPIResourceCommandResult?.stderr}.`);
-        return undefined;
+        return { succeeded: false, error: `Azure Service Operator api-resources command failed with following error: ${asoAPIResourceCommandResult?.stderr}.` };
     } else if (asoAPIResourceCommandResult.code === 0 && !asoAPIResourceCommandResult.stdout) { // No ASO installed.
-        return undefined;
-    } else {
+        return { succeeded: false, error: ASOInstallation.ASONotInstalled };
+    } else { // success sceanrio.
         const treeResourceItems = asoAPIResourceCommandResult.stdout.split("\n").map((line: string) => line.trim()).filter((line: string | any[]) => line.length > 0);
-        return treeResourceItems.map((item: string) => parseServiceResource(item)!);
+        return { succeeded: true, result: treeResourceItems.map((item: string) => parseServiceResource(item)!) };
     }
 }
 
