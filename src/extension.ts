284f9f3e5be9b90f8b76f4f8937d662ec7882597
Tatsinnit <tamishra@microsoft.com>
2021-03-15 14:08:26 +1300

    Update deprecated Azure SDK packages (#61)

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import * as k8s from 'vscode-kubernetes-tools-api';
 import AksClusterTreeItem from './tree/aksClusterTreeItem';
 import AzureAccountTreeItem from './tree/azureAccountTreeItem';
-import { createTelemetryReporter, registerUIExtensionVariables, AzExtTreeDataProvider, AzureUserInput, registerCommand, IActionContext } from 'vscode-azureextensionui';
+import { createAzExtOutputChannel, registerUIExtensionVariables, AzExtTreeDataProvider, AzureUserInput, registerCommand, IActionContext } from 'vscode-azureextensionui';
 import selectSubscriptions from './commands/selectSubscriptions';
 import detectorDiagnostics from './commands/detectorDiagnostics/detectorDiagnostics';
 import periscope from './commands/periscope/periscope';
@@ -22,8 +22,7 @@ export async function activate(context: vscode.ExtensionContext) {
         const uiExtensionVariables = {
             context,
             ignoreBundle: true,
-            outputChannel: vscode.window.createOutputChannel('Azure Identity'),
-            reporter: createTelemetryReporter(context),
+            outputChannel: createAzExtOutputChannel('Azure Identity', ''),
             ui: new AzureUserInput(context.globalState)
         };
 
--- src/tree/aksClusterTreeItem.ts
+++ src/tree/aksClusterTreeItem.ts
@@ -1,11 +1,11 @@
 import { AzExtParentTreeItem, AzureTreeItem, ISubscriptionContext } from "vscode-azureextensionui";
-import { Resource } from "azure-arm-storage/lib/models";
 import { CloudExplorerV1 } from "vscode-kubernetes-tools-api";
-import { SubscriptionClient } from 'azure-arm-resource';
+import { SubscriptionModels } from '@azure/arm-subscriptions';
 import { toSubscription } from "../azure-api-utils";
 import { getExtensionPath } from "../commands/utils/host";
 import * as path from 'path';
 import * as vscode from 'vscode';
+import { Resource } from "@azure/arm-resources/esm/models";
 
 // The de facto API of tree nodes that represent individual AKS clusters.
 // Tree items should implement this interface to maintain backward compatibility with previous versions of the extension.
@@ -14,7 +14,7 @@ export interface AksClusterTreeNode {
     readonly armId: string;
     readonly name: string;
     readonly session: ISubscriptionContext;
-    readonly subscription: SubscriptionClient.SubscriptionModels.Subscription;
+    readonly subscription: SubscriptionModels.Subscription;
 }
 
 export default class AksClusterTreeItem extends AzureTreeItem implements AksClusterTreeNode {
@@ -45,7 +45,7 @@ export default class AksClusterTreeItem extends AzureTreeItem implements AksClus
         return this.root;
     }
 
-    public get subscription(): SubscriptionClient.SubscriptionModels.Subscription {
+    public get subscription(): SubscriptionModels.Subscription {
         return toSubscription(this.root);
     }
 
