7f0581c7fbe26cc418ec141768ce502f6310c1f4
Tatsinnit <tamishra@microsoft.com>
2021-10-19 10:54:40 +1300

    Webpack the extension (#62)

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const fileManagerPlugin = require('filemanager-webpack-plugin');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node',
+  entry: './src/extension.ts',
+  optimization: { 
+    minimize: false
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode',
+    bufferutil: 'commonjs bufferutil',
+    'spawn-sync': 'commonjs spawn-sync',
+    'utf-8-validate': 'commonjs utf-8-validate',
+    'applicationinsights-native-metrics': 'applicationinsights-native-metrics',
+    '@opentelemetry/tracing': '@opentelemetry/tracing'
+  },
+  plugins: [
+      new webpack.IgnorePlugin(/^electron$/),
+      new webpack.IgnorePlugin(/^\.\/locale$/, /handlebars$/),
+      new fileManagerPlugin({
+        events: {
+          onEnd: {
+              copy: [
+                  {
+                      source: path.join(__dirname, 'node_modules', 'vscode-azureextensionui', 'resources', '**'),
+                      destination: path.join(__dirname, 'dist', 'node_modules', 'vscode-azureextensionui', 'resources')
+                  }
+              ]
+          }
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    alias: {
+      'handlebars' : 'handlebars/dist/handlebars.js',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+  }
+};
+module.exports = config;
