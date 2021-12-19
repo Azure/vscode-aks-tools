53b94433f5b8f6ca1856411c2c066c75fbbdc3a4
Tatsinnit <tamishra@microsoft.com>
2021-12-20 01:21:57 +1300

    add mock with sinon and smoke test in CI.

--- src/tests/suite/networkconnecitivityhtmlhelpertest.test.ts
+++ src/tests/suite/networkconnecitivityhtmlhelpertest.test.ts
@@ -1,30 +1,39 @@
-import { convertHtmlJsonConfiguration }  from '../../commands/detectorDiagnostics/helpers/networkconnectivityhtmlhelper';
+import * as networkconnectivityhelper  from '../../commands/detectorDiagnostics/helpers/networkconnectivityhtmlhelper';
 import { expect } from 'chai';
-// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
-import 'mocha';
+import 'sinon';
 import { AppLensARMResponse } from '../../commands/detectorDiagnostics/models/applensarmresponse';
-
-const mockAppLensARMResponse = <AppLensARMResponse> {
-    id: "test",
-    location: "testlocation",
-    name: "testname",
-    properties: JSON.parse(`{
-      "dataset": [
-        {
-          "renderingProperties": "",
-          "description": null,
-          "title": null,
-          "type": 7
-        }
-      ]
-    }`),
-    type: "someTypes",
-    resourceGroup: "testRG"
-};
+import sinon = require('sinon');
 
 describe('Detector Diagnostic Convert HTML JSON Config function Fail case', () => {
   it('should return undefined', () => {
-    const result = convertHtmlJsonConfiguration(mockAppLensARMResponse.properties, 2);
+    const result = networkconnectivityhelper.convertHtmlJsonConfiguration(mockAppLensARMResponse.properties, 2);
     expect(result).to.equal(undefined);
+  });
+});
+const mockAppLensARMResponse = <AppLensARMResponse> {
+  id: "test",
+  location: "testlocation",
+  name: "testname",
+  properties: JSON.parse(`{
+    "dataset": [
+      {
+        "renderingProperties": "",
+        "description": null,
+        "title": null,
+        "type": 7
+      }
+    ]
+  }`),
+  type: "someTypes",
+  resourceGroup: "testRG"
+};
+
+describe("Detector Diagnostic Pass Case", () => {
+  it('should return some value', () => {
+    const stub = sinon.stub(networkconnectivityhelper, 'convertHtmlJsonConfiguration').returns("some value");
+    const result = networkconnectivityhelper.convertHtmlJsonConfiguration(mockAppLensARMResponse.properties, 2);
+
+    expect(result).to.equal("some value");
+    expect(stub.callCount).to.equal(1);
   });
 });
