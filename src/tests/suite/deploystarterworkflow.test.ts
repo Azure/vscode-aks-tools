f226d8bbd7548558dbf0bbbed6f2ce9fefc3242f
Tatsinnit <tamishra@microsoft.com>
2022-04-07 13:06:26 +0530

    Add leftover starter workflow for AKS.

--- src/tests/suite/deploystarterworkflow.test.ts
+++ src/tests/suite/deploystarterworkflow.test.ts
@@ -1,11 +1,11 @@
-import * as configureStarterWorkflow  from '../../commands/aksStarterWorkflow/configureStarterWorkflowHelper';
+import * as configureStarterWorkflow  from '../../commands/utils/configureWorkflowHelper';
 import 'sinon';
 import { expect } from 'chai';
 import { Succeeded, succeeded } from '../../commands/utils/errorable';
 
 describe('Test Configure Starter Data Set returns replaced value.', () => {
   it('should return some string', () => {
-    const result = configureStarterWorkflow.configureStarterConfigDataForAKS("test-resource", "test-cluster");
+    const result = configureStarterWorkflow.configureStarterConfigDataForAKS("test-resource", "test-cluster", "azure-kubernetes-service");
     expect(succeeded(result)).to.be.true;
     expect((result as Succeeded<string>).result).to.contain("test-cluster");
   });
