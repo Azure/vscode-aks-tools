940afcf95933340a974d3de94291c1d9f4faa290
Peter Bomber <peter.bomber@microsoft.com>
2022-04-14 12:07:00 +1200

    add tests to validate expected placeholders in yaml starter workflow templates

--- src/tests/suite/deploystarterworkflow.test.ts
+++ src/tests/suite/deploystarterworkflow.test.ts
@@ -3,10 +3,43 @@ import 'sinon';
 import { expect } from 'chai';
 import { Succeeded, succeeded } from '../../commands/utils/errorable';
 
-describe('Test Configure Starter Data Set returns replaced value.', () => {
-  it('should return some string', () => {
-    const result = configureStarterWorkflow.configureStarterConfigDataForAKS("test-resource", "test-cluster", "azure-kubernetes-service");
-    expect(succeeded(result)).to.be.true;
-    expect((result as Succeeded<string>).result).to.contain("test-cluster");
+describe('Test getWorkflowYaml for missing file', () => {
+  it('should return an error', () => {
+    const result = configureStarterWorkflow.getWorkflowYaml("this-template-does-not-exist");
+    expect(succeeded(result)).to.be.false;
+  });
+});
+
+describe('Test getWorkflowYaml for known files', () => {
+  it('should return string content with expected placeholders', () => {
+    ["azure-kubernetes-service",
+    "azure-kubernetes-service-helm",
+    "azure-kubernetes-service-kompose",
+    "azure-kubernetes-service-kustomize"].forEach(workflowName => {
+      const result = configureStarterWorkflow.getWorkflowYaml(workflowName);
+      expect(succeeded(result)).to.be.true;
+      expect((result as Succeeded<string>).result).to.contain('RESOURCE_GROUP: "your-resource-group"', `resource group placeholder missing from ${workflowName}`);
+      expect((result as Succeeded<string>).result).to.contain('CLUSTER_NAME: "your-cluster-name"', `cluster name placeholder missing from ${workflowName}`);
+    });
+  });
+});
+
+describe('Test substituteClusterInWorkflowYaml', () => {
+  it('should return arguments in output', () => {
+    const initialYamlContent = `
+env:
+  AZURE_CONTAINER_REGISTRY: "your-azure-container-registry"
+  CONTAINER_NAME: "your-container-name"
+  RESOURCE_GROUP: "your-resource-group"
+  CLUSTER_NAME: "your-cluster-name"
+  IMAGE_PULL_SECRET_NAME: "your-image-pull-secret-name"
+  KUSTOMIZE_PATH: "your-kustomize-path"
+`;
+
+    const testResourceGroup = "test-resource-group";
+    const testClusterName = "test-cluster";
+    const result = configureStarterWorkflow.substituteClusterInWorkflowYaml(initialYamlContent, testResourceGroup, testClusterName);
+    expect(result).to.contain(`RESOURCE_GROUP: "${testResourceGroup}"`);
+    expect(result).to.contain(`CLUSTER_NAME: "${testClusterName}"`);
   });
 });
