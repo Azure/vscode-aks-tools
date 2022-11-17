860405f5da01925454a98d87f49a0bc85e3da260
Peter Bomber <peter.bomber@microsoft.com>
2022-11-18 12:11:27 +1300

    add some comments to auth provider

--- src/commands/utils/authProvider.ts
+++ src/commands/utils/authProvider.ts
@@ -6,15 +6,20 @@ import { Errorable } from "./errorable";
 // https://github.com/Azure/azure-sdk-for-js/blob/5eb19b911388ec4ba830e36934bdb4840ec7977d/sdk/identity/identity/src/credentials/visualStudioCodeCredential.ts#L21
 const AzureAccountClientId = "aebc6443-996d-45c2-90f0-388ff96faa56";
 
-export async function getAksAadAccessToken(cloud: Cloud, serverId: string, refreshToken: string, ): Promise<Errorable<AuthenticationResult>> {
+export async function getAksAadAccessToken(cloud: Cloud, serverId: string, refreshToken: string): Promise<Errorable<AuthenticationResult>> {
+    // The MSAL configuration is for the current application (i.e. VS Code),
+    // even though we will be requesting a token for a different application.
     const clientConfig: Configuration = {
         auth: {
             clientId: AzureAccountClientId,
-            authority: "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47",
+            authority: `${cloud.aadEndpoint}/72f988bf-86f1-41af-91ab-2d7cd011db47`,
         }
     };
     
     const application = new PublicClientApplication(clientConfig);
+
+    // The token needs to have an 'audience' claim whose value matches the 'serverId' value (i.e. the AKS AAD server).
+    // We request that by specifying the serverId value in the scope. See: https://stackoverflow.com/a/67737375
     const request: RefreshTokenRequest = {
         scopes: [`${serverId}/.default`],
         refreshToken
