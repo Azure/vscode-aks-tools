0bc66d4edf20e6057b78ee39496ba65406856da0
peterbom <peter.bomber@microsoft.com>
2023-07-03 11:24:01 +1200

    Add webview component for detectors (#207)

--- src/webview-contract/webviewTypes.ts
+++ src/webview-contract/webviewTypes.ts
@@ -75,3 +75,77 @@ export module PeriscopeTypes {
     export type ToVsCodeCommands = UploadStatusRequest | NodeLogsRequest;
     export type ToWebViewCommands = UploadStatusResponse | NodeLogsResponse;
 }
+
+export module DetectorTypes {
+    export const contentId = "detector";
+
+    export interface InitialState {
+        name: string
+        description: string
+        clusterArmId: string
+        portalReferrerContext: string
+        detectors: SingleDetectorARMResponse[]
+    }
+
+    export interface ARMResponse<TDatasets> {
+        id: string
+        name: string
+        type: string
+        location: string
+        properties: {
+            dataset: TDatasets
+            metadata: {
+                category: string
+                description: string
+                id: string
+                name: string
+                type: string
+            }
+            status: {
+                message: string | null
+                statusId: number
+            }
+        }
+    }
+
+    export interface Column {
+        columnName: string
+        columnType: null
+        dataType: string
+    }
+
+    export interface Dataset<TRenderingProperties> {
+        renderingProperties: TRenderingProperties
+        table: {
+            columns: Column[]
+            rows: any[][]
+            tableName: string
+        }
+    }
+
+    export interface RenderingProperties {
+        description: string | null
+        isVisible: boolean
+        title: string | null
+        type: number
+    }
+
+    export interface CategoryDetectorRenderingProperties extends RenderingProperties {
+        additionalParams: string
+        detectorIds: string[]
+        messageIfCritical: string | null
+        resourceUri: string
+    }
+
+    export type CategoryDataset = Dataset<CategoryDetectorRenderingProperties>;
+
+    export type SingleDataset = Dataset<RenderingProperties>;
+
+    export type CategoryDetectorARMResponse = ARMResponse<(CategoryDataset | SingleDataset)[]>;
+
+    export type SingleDetectorARMResponse = ARMResponse<SingleDataset[]>
+
+    export function isCategoryDataset(dataset: CategoryDataset | SingleDataset): dataset is CategoryDataset {
+        return (dataset as CategoryDataset).renderingProperties.detectorIds !== undefined;
+    }
+}
---
+++ webview-ui/src/Detector/utils.ts
@@ -0,0 +1,81 @@
+import { DetectorTypes } from "../../../src/webview-contract/webviewTypes";
+
+const insightDatasetType = 7;
+
+enum InsightColumnName {
+    Status = "Status",
+    Message = "Message",
+    DataName = "Data.Name",
+    DataValue = "Data.Value"
+}
+
+const insightColumnIndexMap = new Map(Object.values(InsightColumnName).map((name, index) => [name, index]));
+
+export enum Status {
+    Success = 0,
+    Warning,
+    Error
+}
+
+export function getOverallStatus(response: DetectorTypes.SingleDetectorARMResponse): Status {
+    const statuses = response.properties.dataset
+        .filter(d => d.renderingProperties.type === insightDatasetType)
+        .map(getStatusForInsightDataset)
+        .filter(isInsightResult)
+        .map(r => r.status);
+
+    return Math.max(...statuses);
+}
+
+export interface InsightResult {
+    status: Status,
+    message: string
+}
+
+export interface ErrorInfo {
+    error: string
+    data: any
+}
+
+export function isInsightResult(result: InsightResult | ErrorInfo): result is InsightResult {
+    return (result as InsightResult).status !== undefined;
+}
+
+export function getStatusForInsightDataset(dataset: DetectorTypes.SingleDataset): InsightResult | ErrorInfo {
+    // One insight has a single overall `Status` and `Message`, even if it contains several rows.
+    if (dataset.table.rows.length === 0) {
+        return {
+            error: "Expected at least 1 row",
+            data: dataset.table
+        };
+    }
+
+    // Check we have the columns we're expecting.
+    if (dataset.table.columns.length < 4) {
+        return {
+            error: "Expected at least 4 columns",
+            data: dataset.table
+        };
+    }
+
+    Object.values(InsightColumnName).forEach((colName, index) => {
+        const actualColName = dataset.table.columns[index].columnName;
+        if (actualColName !== colName) {
+            return {
+                message: `Expected column ${index} to have name ${colName}, but found ${actualColName}`,
+                data: dataset.table.columns
+            };
+        }
+    });
+
+    const statusColumnIndex = insightColumnIndexMap.get(InsightColumnName.Status)!;
+    const messageColumnIndex = insightColumnIndexMap.get(InsightColumnName.Message)!;
+    const statusValue = dataset.table.rows[0][statusColumnIndex];
+    const message = dataset.table.rows[0][messageColumnIndex];
+    const status =
+        statusValue === "Success" ? Status.Success :
+        statusValue === "Warning" ? Status.Warning :
+        Status.Error;
+
+    return { status, message };
+}
