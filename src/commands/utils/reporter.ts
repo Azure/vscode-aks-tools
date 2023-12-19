ef4813fc64a94ee7fb72335d7d82d798ef4dadf5
peterbom <peter.bomber@microsoft.com>
2023-12-20 09:22:41 +1300

    Use new telemetry library (#379)
    Co-authored-by: Tatsinnit <Tatsinnit@users.noreply.github.com>

--- src/commands/utils/reporter.ts
+++ src/commands/utils/reporter.ts
@@ -1,4 +1,4 @@
-import TelemetryReporter from "vscode-extension-telemetry";
+import TelemetryReporter from "@vscode/extension-telemetry";
 import vscode from "vscode";
 import meta from "../../../package.json";
 
@@ -7,23 +7,6 @@ export let reporter: TelemetryReporter;
 export class Reporter extends vscode.Disposable {
     constructor() {
         super(() => reporter.dispose());
-        const packageInfo = getPackageInfo();
-        if (packageInfo) {
-            reporter = packageInfo && new TelemetryReporter(packageInfo.name, packageInfo.version, packageInfo.aiKey);
-        }
+        reporter = new TelemetryReporter(meta.aiKey);
     }
-}
-
-interface IPackageInfo {
-    name: string;
-    version: string;
-    aiKey: string;
-}
-
-function getPackageInfo(): IPackageInfo | undefined {
-    const extensionPackage = meta;
-    if (extensionPackage) {
-        return { name: extensionPackage.name, version: extensionPackage.version, aiKey: extensionPackage.aiKey };
-    }
-    return;
 }
