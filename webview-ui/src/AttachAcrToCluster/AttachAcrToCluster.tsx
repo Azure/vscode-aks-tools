9ce28f54a98a465d1f0d6f31a9ca62b4c5bff467
Peter Bomber <peter.bomber@microsoft.com>
2024-06-17 09:28:13 +1200

    make not-loaded fields disabled and add text to buttons

--- webview-ui/src/AttachAcrToCluster/AttachAcrToCluster.tsx
+++ webview-ui/src/AttachAcrToCluster/AttachAcrToCluster.tsx
@@ -122,73 +122,57 @@ export function AttachAcrToCluster(initialState: InitialState) {
                     onSelect={eventHandlers.onSetSelectedSubscription}
                 />
 
-                {isLoaded(lazyAcrResourceGroups) && (
-                    <>
-                        <label htmlFor="acr-rg-input" className={styles.label}>
-                            ACR Resource Group
-                        </label>
-                        <ResourceSelector<string>
-                            id="acr-rg-input"
-                            className={styles.control}
-                            resources={lazyAcrResourceGroups}
-                            selectedItem={state.selectedAcrResourceGroup}
-                            valueGetter={(g) => g}
-                            labelGetter={(g) => g}
-                            onSelect={eventHandlers.onSetSelectedAcrResourceGroup}
-                        />
-                    </>
-                )}
+                <label htmlFor="acr-rg-input" className={styles.label}>
+                    ACR Resource Group
+                </label>
+                <ResourceSelector<string>
+                    id="acr-rg-input"
+                    className={styles.control}
+                    resources={lazyAcrResourceGroups}
+                    selectedItem={state.selectedAcrResourceGroup}
+                    valueGetter={(g) => g}
+                    labelGetter={(g) => g}
+                    onSelect={eventHandlers.onSetSelectedAcrResourceGroup}
+                />
 
-                {isLoaded(lazyAcrs) && (
-                    <>
-                        <label htmlFor="acr-input" className={styles.label}>
-                            Container Registry
-                        </label>
-                        <ResourceSelector<Acr>
-                            id="acr-input"
-                            className={styles.control}
-                            resources={lazyAcrs}
-                            selectedItem={state.selectedAcr}
-                            valueGetter={(acr) => acr.acrName}
-                            labelGetter={(acr) => acr.acrName}
-                            onSelect={eventHandlers.onSetSelectedAcr}
-                        />
-                    </>
-                )}
+                <label htmlFor="acr-input" className={styles.label}>
+                    Container Registry
+                </label>
+                <ResourceSelector<Acr>
+                    id="acr-input"
+                    className={styles.control}
+                    resources={lazyAcrs}
+                    selectedItem={state.selectedAcr}
+                    valueGetter={(acr) => acr.acrName}
+                    labelGetter={(acr) => acr.acrName}
+                    onSelect={eventHandlers.onSetSelectedAcr}
+                />
 
-                {isLoaded(lazyClusterResourceGroups) && (
-                    <>
-                        <label htmlFor="cluster-rg-input" className={styles.label}>
-                            Cluster Resource Group
-                        </label>
-                        <ResourceSelector<string>
-                            id="cluster-rg-input"
-                            className={styles.control}
-                            resources={lazyClusterResourceGroups}
-                            selectedItem={state.selectedClusterResourceGroup}
-                            valueGetter={(g) => g}
-                            labelGetter={(g) => g}
-                            onSelect={eventHandlers.onSetSelectedClusterResourceGroup}
-                        />
-                    </>
-                )}
+                <label htmlFor="cluster-rg-input" className={styles.label}>
+                    Cluster Resource Group
+                </label>
+                <ResourceSelector<string>
+                    id="cluster-rg-input"
+                    className={styles.control}
+                    resources={lazyClusterResourceGroups}
+                    selectedItem={state.selectedClusterResourceGroup}
+                    valueGetter={(g) => g}
+                    labelGetter={(g) => g}
+                    onSelect={eventHandlers.onSetSelectedClusterResourceGroup}
+                />
 
-                {isLoaded(lazyClusters) && (
-                    <>
-                        <label htmlFor="cluster-input" className={styles.label}>
-                            Cluster
-                        </label>
-                        <ResourceSelector<Cluster>
-                            id="cluster-input"
-                            className={styles.control}
-                            resources={lazyClusters}
-                            selectedItem={state.selectedCluster}
-                            valueGetter={(c) => c.clusterName}
-                            labelGetter={(c) => c.clusterName}
-                            onSelect={eventHandlers.onSetSelectedCluster}
-                        />
-                    </>
-                )}
+                <label htmlFor="cluster-input" className={styles.label}>
+                    Cluster
+                </label>
+                <ResourceSelector<Cluster>
+                    id="cluster-input"
+                    className={styles.control}
+                    resources={lazyClusters}
+                    selectedItem={state.selectedCluster}
+                    valueGetter={(c) => c.clusterName}
+                    labelGetter={(c) => c.clusterName}
+                    onSelect={eventHandlers.onSetSelectedCluster}
+                />
 
                 <label className={styles.label}>Authorization</label>
                 <div className={`${styles.control} ${styles.actionItemList}`}>
@@ -299,11 +283,11 @@ function ActionItem(props: ActionItemProps) {
                         appearance="secondary"
                         onClick={action.action}
                         disabled={!action.canPerformAction}
-                        title={action.name}
                     >
                         <span className={styles.inlineIcon}>
                             <FontAwesomeIcon icon={action.icon} />
-                        </span>
+                        </span>{" "}
+                        {action.name}
                     </VSCodeButton>
                 ))}
             </div>
