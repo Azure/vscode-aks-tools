b6514d02ecb550908def9ba8b9d845caf9f22cc1
Tatsat Mishra <tatsat.mishra@microsoft.com>
2024-07-29 15:53:08 +1200

    intial commit for workspace yaml geenration.
    Signed-off-by: Tatsat Mishra <tatsat.mishra@microsoft.com>

---
+++ src/commands/utils/configureWorkflowHelper.ts
@@ -0,0 +1,28 @@
+import path from "path";
+import * as fs from "fs";
+import { getExtensionPath } from "./host";
+import * as vscode from "vscode";
+import { Errorable, failed } from "./errorable";
+
+export function getWorkflowYaml(workflowName: string): Errorable<string> {
+    const extensionPath = getExtensionPath();
+    if (failed(extensionPath)) {
+        return extensionPath;
+    }
+
+    const yamlPathOnDisk = vscode.Uri.file(path.join(extensionPath.result, "resources", "yaml", `${workflowName}.yaml`));
+    try {
+        const content = fs.readFileSync(yamlPathOnDisk.fsPath, "utf8");
+        return { succeeded: true, result: content };
+    } catch (e) {
+        return { succeeded: false, error: `Failed to read ${yamlPathOnDisk}: ${e}` };
+    }
+}
+
+export function substituteClusterInWorkflowYaml(
+    workflowYaml: string,
+    instanceType: string,
+    appID: string,
+): string {
+    return workflowYaml.replace("<INSTANCE_TYPE>", instanceType).replaceAll("<APP_ID>", appID);
+}
