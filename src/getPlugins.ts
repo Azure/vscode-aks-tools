b63c8534a62c8c833e819da2d0ad009124964fe6
Kevin Jaturapatporn (from Dev Box) <kejatura@microsoft.com>
2024-08-14 09:36:49 -0700

    refactor and update ARM template for AKS deployment

--- src/getPlugins.ts
+++ src/getPlugins.ts
@@ -1,6 +1,6 @@
 import { type IActionContext } from "@microsoft/vscode-azext-utils";
 import { GetPluginsCommandResult } from "copilot-for-azure-vscode-api";
-import { deployAppToAKSPlugin } from "./plugins/deployAppToAKSPlugin";
+import { deployAppToAKSPlugin } from "./plugins/deployAppToAKSPlugin/deployAppToAKSPlugin";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function getPlugins(_actionContext: IActionContext): Promise<GetPluginsCommandResult> {
---
+++ src/plugins/deployAppToAKSPlugin/deployAppToAKSPluginHelper.ts
@@ -0,0 +1,38 @@
+import { RestError } from "@azure/storage-blob";
+import { getErrorMessage } from "../../commands/utils/errorable";
+
+export  type InvalidTemplateDeploymentRestError = RestError & {
+    details: {
+        error?: {
+            code: "InvalidTemplateDeployment";
+            message?: string;
+            details?: {
+                code?: string;
+                message?: string;
+            }[];
+        };
+    };
+};
+
+export function getInvalidTemplateErrorMessage(ex: InvalidTemplateDeploymentRestError): string {
+    const innerDetails = ex.details.error?.details || [];
+    if (innerDetails.length > 0) {
+        const details = innerDetails.map((d) => `${d.code}: ${d.message}`).join("\n");
+        return `Invalid template:\n${details}`;
+    }
+
+    const innerError = ex.details.error?.message || "";
+    if (innerError) {
+        return `Invalid template:\n${innerError}`;
+    }
+
+    return `Invalid template: ${getErrorMessage(ex)}`;
+}
+
+export function isInvalidTemplateDeploymentError(ex: unknown): ex is InvalidTemplateDeploymentRestError {
+    return isRestError(ex) && ex.code === "InvalidTemplateDeployment";
+}
+
+export  function isRestError(ex: unknown): ex is RestError {
+    return typeof ex === "object" && ex !== null && ex.constructor.name === "RestError";
+}
