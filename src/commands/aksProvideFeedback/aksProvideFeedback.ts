a782a22a58d6cd980ec365b88655379109c105eb
Reinier Cruz <rcruzcarnero@microsoft.com>
2024-08-29 11:15:15 -0400

    Removing unused context

--- src/commands/aksProvideFeedback/aksProvideFeedback.ts
+++ src/commands/aksProvideFeedback/aksProvideFeedback.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
-import { IActionContext } from "@microsoft/vscode-azext-utils";
 
-export default async function aksProvideFeedback(_context: IActionContext): Promise<void> {
+export default async function aksProvideFeedback(): Promise<void> {
     await vscode.commands.executeCommand("workbench.action.openIssueReporter", {
         extensionId: "ms-kubernetes-tools.vscode-aks-tools",
     });
