c4b09d93d45c5d97fd7dfd9dea4df2e81e31d1ed
Hariharan Subramanian <hsubramanian@microsoft.com>
2024-09-19 16:47:25 -0400

    ux changes

--- webview-ui/src/CreateCluster/CreateClusterPresetInput.tsx
+++ webview-ui/src/CreateCluster/CreateClusterPresetInput.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Preset } from "../../../src/webview-contract/webviewDefinitions/createCluster";
 import { DevTestIcon } from "../icons/DevTestIcon";
 import styles from "./CreateCluster.module.css";
@@ -7,9 +8,11 @@ export interface CreateClusterPresetInputProps {
 }
 
 export function CreateClusterPresetInput(props: CreateClusterPresetInputProps) {
+    const [selectedPreset, setSelectedPreset] = useState<string | null>(null);
     function handlePresetClick(presetSelected: Preset) {
         console.log(presetSelected);
         props.onPresetSelected(presetSelected);
+        setSelectedPreset(presetSelected);
     }
 
     return (
@@ -23,38 +26,44 @@ export function CreateClusterPresetInput(props: CreateClusterPresetInputProps) {
                     <a href="https://portal.azure.com/#create/Microsoft.AKS">click here</a>
                     &nbsp;to visit the Azure Portal.
                 </div>
-                <div style={{ display: 'flex' }}> 
-                <div className={styles.presetContainer} onClick={() => handlePresetClick("dev")}>
-                    <div className={styles.flexContainer}>
-                        <DevTestIcon className={styles.svgContainer} style={{ width: "1rem", height: "1rem" }} />
-                        <div className={styles.presetTitle}>Dev/Test</div>
-                    </div>
-                    <div className={styles.presetDescription}>
-                        Best for developing new workloads or testing existing workloads.
-                        <div className={styles.learnMoreContainer}>
-                            <a href="https://learn.microsoft.com/en-us/azure/aks/quotas-skus-regions#cluster-configuration-presets-in-the-azure-portal">
-                                Learn more
-                            </a>
+                <div style={{ display: "flex" }}>
+                    <div
+                        className={`${styles.presetContainer} ${selectedPreset === "dev" ? styles.presetContainerHighlighted : ""}`}
+                        onClick={() => handlePresetClick("dev")}
+                    >
+                        <div className={styles.flexContainer}>
+                            <DevTestIcon className={styles.svgContainer} style={{ width: "1rem", height: "1rem" }} />
+                            <div className={styles.presetTitle}>Dev/Test</div>
+                        </div>
+                        <div className={styles.presetDescription}>
+                            Best for developing new workloads or testing existing workloads.
+                            <div className={styles.learnMoreContainer}>
+                                <a href="https://learn.microsoft.com/en-us/azure/aks/quotas-skus-regions#cluster-configuration-presets-in-the-azure-portal">
+                                    Learn more
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className={styles.presetContainer} onClick={() => handlePresetClick("automatic")}>
-                    <div className={styles.flexContainer}>
-                        <DevTestIcon className={styles.svgContainer} style={{ width: "1rem", height: "1rem" }} />
-                        <div className={styles.presetTitle}>Automatic</div>
-                    </div>
-                    <div className={styles.presetDescription}>
-                    Best for production ready that automatically configures the cluster with recommended settings.
-                        <div className={styles.learnMoreContainer}>
-                            <a href="https://learn.microsoft.com/en-us/azure/aks/learn/quick-kubernetes-automatic-deploy?pivots=azure-portal">
-                                Learn more
-                            </a>
+                    <div
+                        className={`${styles.presetContainer} ${selectedPreset === "automatic" ? styles.presetContainerHighlighted : ""}`}
+                        onClick={() => handlePresetClick("automatic")}
+                    >
+                        <div className={styles.flexContainer}>
+                            <DevTestIcon className={styles.svgContainer} style={{ width: "1rem", height: "1rem" }} />
+                            <div className={styles.presetTitle}>Automatic</div>
+                        </div>
+                        <div className={styles.presetDescription}>
+                            Best for production ready that automatically configures the cluster with recommended
+                            settings.
+                            <div className={styles.learnMoreContainer}>
+                                <a href="https://learn.microsoft.com/en-us/azure/aks/learn/quick-kubernetes-automatic-deploy?pivots=azure-portal">
+                                    Learn more
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-                </div>
-               
             </div>
         </>
     );
