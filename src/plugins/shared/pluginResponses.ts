d92d6254dac71ca39e584edced352a3c30fd61c2
Kevin Jaturapatporn (from Dev Box) <kejatura@microsoft.com>
2024-10-09 12:05:11 -0700

    update plugin response

--- src/plugins/shared/pluginResponses.ts
+++ src/plugins/shared/pluginResponses.ts
@@ -1,22 +1,34 @@
-type GitHubCopilotForAzureChatPluginResponse = {
-    messageForLanguageModel: string;
-    buttonLabel: string;
-    commandID: string;
-}
+import { MessageForLanguageModel, GitHubCopilotForAzureChatPluginResponse} from "./types";
 
 // Create AKS cluster plugin
-export const getAKSClusterPluginResponse = (): GitHubCopilotForAzureChatPluginResponse => {
+export const getAKSClusterPluginResponse = (): GitHubCopilotForAzureChatPluginResponse<MessageForLanguageModel> => {
+    const messageForLanguageModel = {
+        description: "To create a new AKS cluster, please use the Azure Kubernetes Service (AKS) extension in Visual Studio Code. This extension provides a guided experience, making it easier to configure your cluster. Follow the extension's instructions to complete the setup.",
+        descriptionInstructions: "Focus on the description of the message, no need to add any other text.",
+    }
+
     return {
-        messageForLanguageModel: "To create a new AKS cluster, please use the Azure Kubernetes Service (AKS) extension in Visual Studio Code. This extension provides a guided experience, making it easier to configure your cluster. Follow the extension's instructions to complete the setup.",
+        messageForLanguageModel: messageForLanguageModel,
         commandID: "aks.aksCreateClusterFromCopilot",
         buttonLabel: "Get started"
     }
 }
 
 //Deploy manifest to AKS cluster plugin
-export const getDeployManifestToAKSPluginResponse = (): GitHubCopilotForAzureChatPluginResponse => {
+export const getDeployManifestToAKSPluginResponse = (): GitHubCopilotForAzureChatPluginResponse<MessageForLanguageModel> => {
+
+    const messageForLanguageModel = {
+        description: "To deploy an application to an AKS cluster, you will need to do the following:",
+        steps: [
+            "Select the Kubernetes manifest files in your application.",
+            "Choose an existing cluster or create a new AKS Automatic or Dev/Test - (hint: add bullet) If you choose to create a new cluster, a page will guide you. (hint: add bullet) Once done, click 'Get Started' to continue deploying your application.",
+            "Deploy your application."
+        ],
+        stepsInstructions: "Focus on the steps of the message, no need to add any other text."
+    }
+
     return {
-        messageForLanguageModel: `Absolutely, I'd be happy to assist you with deploying your application to an AKS cluster. To deploy an application to an AKS cluster, you will need to do the following: 1. Select the Kubernetes manifest files in your application. 2. Choose an existing cluster or create a new AKS Automatic or Dev/Test. 2.a. If you choose to create a new cluster, a page will guide you. 2.b. Once done, click 'Get Started' to continue deploying your application. 4. Deploy your application.`,
+        messageForLanguageModel: messageForLanguageModel,
         commandID: "aks.aksDeployManifest",
         buttonLabel: "Get started"
     }
--- src/plugins/shared/types.ts
+++ src/plugins/shared/types.ts
@@ -5,3 +5,25 @@ export type ClusterPreference = {
     resourceGroup: string;
     kubeConfigYAML: string;
 };
+
+export type MessageForLanguageModel = {
+    description?: string;
+    descriptionInstructions?: string;
+    steps?: string[];
+    stepsInstructions?: string;
+    chatResponseInstructions?: string;
+}
+
+export type GitHubCopilotForAzureChatPluginResponse<T> = {
+    messageForLanguageModel: T;
+    buttonLabel: string;
+    commandID: string;
+}
+
+export type GitHubCopilotForAzureChatPluginResponseExtended<E, T> = GitHubCopilotForAzureChatPluginResponse<E> & {
+    arguments: T[] | undefined;
+}
+
+export type GitHubCopilotForAzureChatPluginErrorResponse<T> = {
+    messageForLanguageModel: T;
+}
