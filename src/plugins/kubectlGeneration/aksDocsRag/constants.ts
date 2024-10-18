01a1d1de9ddbe713cc18857520264b63e3c7d253
Kevin Jaturapatporn (from Dev Box) <kejatura@microsoft.com>
2024-10-17 20:44:13 -0700

    fix lint

--- src/plugins/kubectlGeneration/aksDocsRag/constants.ts
+++ src/plugins/kubectlGeneration/aksDocsRag/constants.ts
@@ -7,4 +7,36 @@ export const aksDocsRAGScenario = "Azure Kubernetes Service";
 export const aksDocsRAGIntent = "RCH";
 
 // https://github.com/MicrosoftDocs/azure-docs/blob/main/articles/communication-services/concepts/european-union-data-boundary.md
-export const EUCountries = ["AT", "BE", "BG", "CH", "CY", "CZ", "DE", "DK", "EE", "ES", "FI", "FR", "GR", "HR", "HU", "IE", "IS", "IT", "LI", "LT", "LU", "LV", "MT", "NL", "NO", "PL", "PT", "RO", "SE", "SI", "SK"];
+export const EU_COUNTRIES = [
+    "AT",
+    "BE",
+    "BG",
+    "CH",
+    "CY",
+    "CZ",
+    "DE",
+    "DK",
+    "EE",
+    "ES",
+    "FI",
+    "FR",
+    "GR",
+    "HR",
+    "HU",
+    "IE",
+    "IS",
+    "IT",
+    "LI",
+    "LT",
+    "LU",
+    "LV",
+    "MT",
+    "NL",
+    "NO",
+    "PL",
+    "PT",
+    "RO",
+    "SE",
+    "SI",
+    "SK",
+];
