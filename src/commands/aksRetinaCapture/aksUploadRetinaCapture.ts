b1d4b604ca99de093a3b785456666c076633e5d6
Suneha Bose <suneha.bose@gmail.com>
2025-03-12 01:42:16 -0700

    fix import

--- src/commands/aksRetinaCapture/aksUploadRetinaCapture.ts
+++ src/commands/aksRetinaCapture/aksUploadRetinaCapture.ts
@@ -9,7 +9,7 @@ import { getRetinaBinaryPath } from "../utils/helper/retinaBinaryDownload";
 import { invokeKubectlCommand } from "../utils/kubectl";
 import { RetinaCapturePanel, RetinaCaptureProvider } from "../../panels/RetinaCapturePanel";
 import { failed } from "../utils/errorable";
-import { getClusterDiagnosticSettings, validateClusterPrerequisites as validatePrerequisites } from "../utils/clusters";
+import { getClusterDiagnosticSettings, validatePrerequisites as validatePrerequisites } from "../utils/clusters";
 import { getAksClusterTreeNode } from "../utils/clusters";
 import { getBlobStorageInfo, getSASKey, LinkDuration } from "../utils/azurestorage";
 import { parseResource } from "../../azure-api-utils";
