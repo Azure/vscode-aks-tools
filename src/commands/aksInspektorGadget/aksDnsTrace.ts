f757ec5dfcb408064348d03fbbab97fc34914b07
Suneha Bose <suneha.bose@gmail.com>
2025-06-26 12:30:30 -0700

    rename/change wording

--- src/commands/aksInspektorGadget/aksDnsTrace.ts
+++ src/commands/aksInspektorGadget/aksDnsTrace.ts
@@ -1,9 +1,9 @@
 import { IActionContext } from "@microsoft/vscode-azext-utils";
 import { openInspektorGadgetTrace } from "./inspektorGadgetHelper";
 
-export async function aksDnsDebug(context: IActionContext, target: unknown): Promise<void> {
+export async function aksInvestigateDns(context: IActionContext, target: unknown): Promise<void> {
     await openInspektorGadgetTrace(context, target, {
-        title: "Launching Inspektor Gadget DNS Debug Tool",
+        title: "Launching Inspektor Gadget Tool for DNS Investigation",
         resource: "dns",
     });
 }
--- src/commands/aksInspektorGadget/aksTcpTrace.ts
+++ src/commands/aksInspektorGadget/aksTcpTrace.ts
@@ -1,9 +1,9 @@
 import { IActionContext } from "@microsoft/vscode-azext-utils";
 import { openInspektorGadgetTrace } from "./inspektorGadgetHelper";
 
-export async function aksTcpTrace(context: IActionContext, target: unknown): Promise<void> {
+export async function aksRealTimeTcpMonitoring(context: IActionContext, target: unknown): Promise<void> {
     await openInspektorGadgetTrace(context, target, {
-        title: "Launching Inspektor Gadget TCP Trace",
+        title: "Launching Inspektor Gadget Tool for Real-Time TCP Monitoring",
         resource: "tcp",
     });
 }
