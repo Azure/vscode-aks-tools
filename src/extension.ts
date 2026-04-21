b2d5d39e380d5135bf724411809393c72c2f5bf7
Suneha Bose <suneha.bose@gmail.com>
2026-04-20 17:59:49 -0700

    merge main

--- src/extension.ts
+++ src/extension.ts
@@ -72,7 +72,11 @@ import * as fs from "fs";
 import { addMcpServerToUserSettings } from "./commands/aksMCP/aksMCPServer";
 import { aksQuickActions, initializeQuickActions } from "./commands/quickActions/aksQuickActions";
 import { runContainerAssist, runContainerAssistFromTree } from "./commands/aksContainerAssist/aksContainerAssist";
-import { containerizationApp, migrateAndModernizeApp } from "./commands/aksContainerAssist/appModernizationBridge";
+import {
+    containerizationApp,
+    deployAppWithAutomatedPipeline,
+    migrateAndModernizeApp,
+} from "./commands/aksContainerAssist/appModernizationBridge";
 import {
     setupOIDCForGitHub,
     setGitHubActionsSecrets,
@@ -169,6 +173,7 @@ export async function activate(context: vscode.ExtensionContext) {
         registerCommandWithTelemetry("aks.runContainerAssist", runContainerAssist);
         registerCommandWithTelemetry("aks.runContainerAssistFromTree", runContainerAssistFromTree);
         registerCommandWithTelemetry("aks.containerizationApp", containerizationApp);
+        registerCommandWithTelemetry("aks.deployAppWithAutomatedPipeline", deployAppWithAutomatedPipeline);
         registerCommandWithTelemetry("aks.migrateAndModernizeApp", migrateAndModernizeApp);
         registerCommandWithTelemetry("aks.setupOIDCForGitHub", async () => {
             const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
